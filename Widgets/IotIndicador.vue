1cd04ff6beb7556e3729e06ff171966da8def507
Fco Javier <fran.merchan6@gmail.com>
2022-08-09 12:44:53 +0200

    Maqueta Iot Indicador funcionando

---
+++ Widgets/IotIndicador.vue
@@ -0,0 +1,70 @@
+<template>
+    <card>
+        <div slot="header">
+            <h4 class="card-title">{{config.selectedDevice.name}} - {{config.variableFullName}}</h4>
+        </div>
+
+        <i class="fa " :class="[config.icon, getIconColorClass() ]" style="font-size: 30px "></i>
+    </card>
+</template>
+
+<script>
+
+export default {
+    // props: ['config'],
+    data(){
+        return{
+            value: TextTrackCueList,//false, //valor del icono
+            config:{
+                userId: 'userid',
+                selectedDevice: {
+                    name: "Hogar",
+                    dID: "9874",
+                    templateName: "Senores",
+                    templateID: "46468464",
+                    saverRule:false,
+                },
+                variableFullName: "Pump",
+                variable: "ClaveUnicaString",
+                icon: "fa-sun",
+                column: 'col-6',
+                widgetType: "indicator",
+                class: 'danger'
+            }
+        }
+    },
+    mounted(){
+        this.$nuxt.$on('widget-tipic', this.procesadoDatosRecibidos)
+    },
+    methods: {
+
+        procesadoDatosRecibidos(data){
+            console.log("Recibido");
+            console.log(data);
+            this.value = data.value;
+        },
+
+        getIconColorClass(){
+            if(!this.value){
+                return "text-dark"
+            }
+            // if (this.config.class == "success"){
+            //     return "text-success";
+            // }
+            // if(this.config.class == "primary"){
+            //     return "text-primary";
+            // }
+            // if(this.config.class == "warning"){
+            //     return "text-warning";
+            // }
+            // if(this.config.class == "danger"){
+            //     return "text-danger";
+            // }
+
+            //version corta -TEST
+            return `text-${this.config.class}`;
+        }
+    }
+}
+</script>
+
--- pages/templates.vue
+++ pages/templates.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
         <h2> Templates</h2>
+
+        <iot-indicador></iot-indicador>
+        <button @click="sendData()">Send</button>
+
     </div>
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                value:false
+            }
+        },
+        methods: {
+            sendData(){
+                this.value = !this.value;
+                const toSend ={
+                    value: this.value
+                }
+                this.$nuxt.$emit('widget-tipic', toSend);
+
+            }
+        }
+    }
+</script>
